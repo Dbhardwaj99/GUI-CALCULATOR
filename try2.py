d7d535a157d967fa8375bda12e0b0d1fc24a6a0a
Divyansh Bhardwaj <87605149+Dbhardwaj99@users.noreply.github.com>
2021-10-31 23:48:20 +0530

    Added the add functions
    Now you can add numbers

--- try2.py
+++ try2.py
@@ -1,3 +1,4 @@
+import copy
 from tkinter import *
 
 root = Tk()
@@ -6,45 +7,60 @@ root.title("Calculator By DIVYANSH")
 
 
 e = Entry(root, fg="red", width=30, borderwidth=5)
-e.grid(row=0, column=0, columnspan=4, padx=10, pady= 10)
+e.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
+# num1 = 0
 
 def button_click(number):
     current = e.get()
     e.delete(0, END)
     e.insert(0, str(current) + str(number))
 
+def button_add():
+    cur1 = e.get()
+    global num1
+    num1 = int(cur1[:])
+    e.delete(0, END)
+
+def button_equal():
+    print(num1)
+    cur2 = e.get()
+    e.delete(0, END)
+    num2 = int(cur2)
+    sum_of_it = num1+num2
+    e.insert(0, str(sum_of_it))
+
 def clear():
     e.delete(0, END)
 
 
-buttoon_1 = Button(root, text="1", padx=40, pady=20, command=lambda : button_click(1))
-buttoon_2 = Button(root, text="2", padx=40, pady=20, command=lambda : button_click(2))
-buttoon_3 = Button(root, text="3", padx=40, pady=20, command=lambda : button_click(3))
-buttoon_4 = Button(root, text="4", padx=40, pady=20, command=lambda : button_click(4))
-buttoon_5 = Button(root, text="5", padx=40, pady=20, command=lambda : button_click(5))
-buttoon_6 = Button(root, text="6", padx=40, pady=20, command=lambda : button_click(6))
-buttoon_7 = Button(root, text="7", padx=40, pady=20, command=lambda : button_click(7))
-buttoon_8 = Button(root, text="8", padx=40, pady=20, command=lambda : button_click(8))
-buttoon_9 = Button(root, text="9", padx=40, pady=20, command=lambda : button_click(9))
-buttoon_0 = Button(root, text="0", padx=40, pady=20, command=lambda : button_click(0))
-buttoon_add = Button(root, text="+", padx=39, pady=20, command=lambda : button_click())
-buttoon_equal = Button(root, text="=", padx=180, pady=20, command=lambda : button_click())
-buttoon_clear = Button(root, text="C", padx=39, pady=20, command=clear)
+button_1 = Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1))
+button_2 = Button(root, text="2", padx=40, pady=20, command=lambda: button_click(2))
+button_3 = Button(root, text="3", padx=40, pady=20, command=lambda: button_click(3))
+button_4 = Button(root, text="4", padx=40, pady=20, command=lambda: button_click(4))
+button_5 = Button(root, text="5", padx=40, pady=20, command=lambda: button_click(5))
+button_6 = Button(root, text="6", padx=40, pady=20, command=lambda: button_click(6))
+button_7 = Button(root, text="7", padx=40, pady=20, command=lambda: button_click(7))
+button_8 = Button(root, text="8", padx=40, pady=20, command=lambda: button_click(8))
+button_9 = Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
+button_0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
+button_sum = Button(root, text="+", padx=39, pady=20, command=button_add)
+button_equal = Button(root, text="=", padx=180, pady=20, command=button_equal)
+button_clear = Button(root, text="C", padx=39, pady=20, command=clear)
 
-buttoon_1.grid(row= 3,column= 0)
-buttoon_2.grid(row= 3,column= 1)
-buttoon_3.grid(row= 3,column= 2)
-buttoon_0.grid(row= 3,column= 3)
+button_1.grid(row=3, column=0)
+button_2.grid(row=3, column=1)
+button_3.grid(row=3, column=2)
+button_0.grid(row=3, column=3)
 
-buttoon_4.grid(row= 2,column= 0)
-buttoon_5.grid(row= 2,column= 1)
-buttoon_6.grid(row= 2,column= 2)
-buttoon_add.grid(row=2,column=3)
+button_4.grid(row=2, column=0)
+button_5.grid(row=2, column=1)
+button_6.grid(row=2, column=2)
+button_sum.grid(row=2, column=3)
 
-buttoon_7.grid(row= 1,column= 0)
-buttoon_8.grid(row= 1,column= 1)
-buttoon_9.grid(row= 1,column= 2)
-buttoon_clear.grid(row=1,column=3)
+button_7.grid(row=1, column=0)
+button_8.grid(row=1, column=1)
+button_9.grid(row=1, column=2)
+button_clear.grid(row=1, column=3)
 
-buttoon_equal.grid(row=4,column=0,columnspan=4)
+button_equal.grid(row=4, column=0, columnspan=4)
 root.mainloop()
